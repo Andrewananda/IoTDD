21339b060fb52ea332d4fee088318da83a09700b
andrew.tiema <andrewtiema@gmail.com>
2021-05-07 15:59:35 +0300

    test

--- ToDoTests/ToDoTests.swift
+++ ToDoTests/ToDoTests.swift
@@ -23,16 +23,17 @@ class ToDoTests: XCTestCase {
         XCTAssertNotNil(item, "Item Should not be nill")
     }
 
-//    func testExample() throws {
-//        // This is an example of a functional test case.
-//        // Use XCTAssert and related functions to verify your tests produce the correct results.
-//    }
-//
-//    func testPerformanceExample() throws {
-//        // This is an example of a performance test case.
-//        self.measure {
-//            // Put the code you want to measure the time of here.
-//        }
-//    }
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
 
 }
